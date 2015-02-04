5c2c6f6d55289d699537ea52e9efe9a597cd41da
potzenhotz <lukasmuessle@gmail.com>
2015-02-04 14:22:21 +0100

    made a new python tkstat plot

--- master/tkstat/general_ar_re200/plot_tkstat.py
+++ master/tkstat/general_ar_re200/plot_tkstat.py
@@ -38,18 +38,30 @@ rcParams.update({'figure.autolayout':True})
 #Load Data
 ########################################################################
 
-data_name='general_ar_re200_avg3.dat'
-avg3_time, avg3_value = np.loadtxt(data_name, unpack=True)
+#data_name='general_ar_re200_avg3.dat'
+#data_name='general_ar_re200_avg3_t2000.dat'
+#avg3_time, avg3_value = np.loadtxt(data_name, unpack=True)
+#
+#data_name='general_ar_re200_avg5.dat'
+#avg5_time, avg5_value = np.loadtxt(data_name, unpack=True)
+#
+#data_name='general_ar_re200_avg6.dat'
+#data_name='general_ar_re200_avg6_t2000.dat'
+#avg6_time, avg6_value = np.loadtxt(data_name, unpack=True)
+#
+#avg3_time=avg3_time*50/60
+#avg5_time=avg5_time*50/60
+#avg6_time=avg6_time*50/60
 
-data_name='general_ar_re200_avg5.dat'
-avg5_time, avg5_value = np.loadtxt(data_name, unpack=True)
+#
+#LOAD DIFFERENT SET
+#
+data_name='general_ar_re200_avg3_t2000.dat'
+avg3_y, avg3_value = np.loadtxt(data_name, unpack=True)
 
-data_name='general_ar_re200_avg6.dat'
-avg6_time, avg6_value = np.loadtxt(data_name, unpack=True)
+data_name='general_ar_re200_avg6_t2000.dat'
+avg6_y, avg6_value = np.loadtxt(data_name, unpack=True)
 
-avg3_time=avg3_time*50/60
-avg5_time=avg5_time*50/60
-avg6_time=avg6_time*50/60
 
 
 ########################################################################
@@ -61,15 +73,22 @@ fig, ax = ppl.subplots()
 color_set = brewer2mpl.get_map('paired', 'qualitative', 12).mpl_colors
 color_set_2 = brewer2mpl.get_map('PuBu', 'sequential', 9).mpl_colors
 
-ax.plot(avg5_time,avg5_value , color=color_set[3], label='Parcel Liquid', linestyle='-',linewidth=l_w_1)
-ax.plot(avg6_time,avg6_value , color=color_set[5], label='Droplet Liquid', linestyle='-',linewidth=l_w_1)
+#ax.plot(avg5_time,avg5_value , color=color_set[3], label='Parcel Liquid', linestyle='-',linewidth=l_w_1)
+#ax.plot(avg6_time,avg6_value , color=color_set[5], label='Droplet Liquid', linestyle='-',linewidth=l_w_1)
+#plt.xlabel('Time', fontsize=size_axes)#,fontdict=font) #das r hier markiert, dass jetz latex code kommt
+#plt.ylabel('Differences', fontsize=size_axes)#,fontdict=font)
+#plt.xlim(0,30)
+#plt.ylim([0.02,0.1])
+#plt.legend(loc=1, frameon=False)
 
+ax.plot(avg3_value, avg3_y, color=color_set[3], label='Liquid', linestyle='-',linewidth=l_w_1)
+ax.plot(avg6_value, avg6_y, color=color_set[5], label='Droplet Liquid', linestyle='-',linewidth=l_w_1)
 
-plt.xlabel('Time', fontsize=size_axes)#,fontdict=font) #das r hier markiert, dass jetz latex code kommt
-plt.ylabel('Differences', fontsize=size_axes)#,fontdict=font)
-plt.xlim(0,30)
-plt.ylim([0.02,0.1])
-plt.legend(loc=1, frameon=False)
+plt.xlabel('Liquid', fontsize=size_axes)#,fontdict=font) #das r hier markiert, dass jetz latex code kommt
+plt.ylabel('Height in lambda', fontsize=size_axes)#,fontdict=font)
+plt.xlim(0.8,1.3)
+plt.ylim(40,70)
+plt.legend(loc=4, frameon=False)
 
 plt.show()
 
