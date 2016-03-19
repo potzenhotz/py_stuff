d5f85b9b8a999f8a546b1227bd640831492f2f4c
potzenhotz <lukasmuessle@gmail.com>
2016-03-19 16:28:58 +0100

    DEAL WITH IT

--- weather_program/weather_program.py
+++ weather_program/weather_program.py
@@ -30,37 +30,40 @@ user_cities = {'Lukas': lukas_cities
             ,'Alex': alex_cities
             }
 
-#-----------------------------------------------------------------------
-#Testing area
-#-----------------------------------------------------------------------
-test_flag = 0
+users = ['Lukas']
 
-if test_flag == 1:
-  #for keys in city_dict:
-  #  ws.current_weather(city_dict[keys])
-  print(ws.current_weather(city_dict['Koeln']))
-
-else:  
+for user in users:
   #-----------------------------------------------------------------------
-  #Email configuration
+  #Testing area
   #-----------------------------------------------------------------------
-  pwd_file_name = 'python_mailing_bot.txt'
-  pwd_file_loc = os.environ['HOME'] + '/Documents/'
- 
-  #reads first line of the file 
-  pwd_mail = us.read_certain_line(pwd_file_name,pwd_file_loc,0)
+  test_flag = 0
   
-  email_dict_keys_list = list(email_dict.keys())
-  to_addrs  = email_dict['Lukas']
-
-  #setup the email subject
-  subject = 'Wetterbericht fuer ' + str(email_dict_keys_list[0]) + ' [PROTOTYP]'
-  #body of email
-  body_msg = 'Diese Staedte hast du aboniert:'
-  for cities_var in user_cities['Lukas']:
-    body_msg += '\n' + str(ws.current_weather(city_dict[cities_var]))
-
-  #send actual mail 
-  us.send_mail(to_addrs, subject, body_msg, pwd_mail)
-
+  if test_flag == 1:
+    #for keys in city_dict:
+    #  ws.current_weather(city_dict[keys])
+    print(ws.current_weather(city_dict['Koeln']))
+  
+  else:  
+    #-----------------------------------------------------------------------
+    #Email configuration
+    #-----------------------------------------------------------------------
+    pwd_file_name = 'python_mailing_bot.txt'
+    pwd_file_loc = os.environ['HOME'] + '/Documents/'
+   
+    #reads first line of the file 
+    pwd_mail = us.read_certain_line(pwd_file_name,pwd_file_loc,0)
+    
+    email_dict_keys_list = list(email_dict.keys())
+    to_addrs  = email_dict[user]
+  
+    #setup the email subject
+    subject = 'Wetterbericht fuer ' + str(user) + ' [PROTOTYP]'
+    #body of email
+    body_msg = 'Diese Staedte hast du aboniert:'
+    for cities_var in user_cities[user]:
+      body_msg += '\n' + str(ws.current_weather(city_dict[cities_var]))
+  
+    #send actual mail 
+    us.send_mail(to_addrs, subject, body_msg, pwd_mail)
+  
 
