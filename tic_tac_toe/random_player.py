669df99d0f29d9e7a4ecbfc0261d04832797f642
potzenhotz <lukasmuessle@gmail.com>
2016-11-01 10:26:48 +0100

    DEAL WITH IT

--- tic_tac_toe/random_player.py
+++ tic_tac_toe/random_player.py
@@ -1,30 +1,33 @@
 #!/bin/env python3
-#Random player for tic tac toe
+'''
+Random player for tic tac toe
+'''
 
 import random
 
-class randomPlayer(object):
+
+class RandomPlayer(object):
 
     def __init__(self, player, game):
-        self.setPlayer(player)        
+        self.set_player(player)
         self.game = game
 
-    def setPlayer(self, player):
+    def set_player(self, player):
         self.Player = player
 
-    def makeMove(self, game, position):
+    def make_move(self, game, position):
         game.setMove(position, self.Player)
 
     def getField(self, game):
         self.field = game.field[:]
-        
+
     def calcPossibleMoves(self):
         self.getField(self.game)
         self.possibleMoves = [
             idx for idx, value in enumerate(self.field) if value == 0
             ]
-    
+
     def makeRandomMove(self):
         self.calcPossibleMoves()
         randomMove = random.choice(self.possibleMoves)
-        self.makeMove(self.game, randomMove)
+        self.make_move(self.game, randomMove)
